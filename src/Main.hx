afb264183fa96ff021cbd9b409087e449525ddd9
Caue Waneck <waneck@gmail.com>
2013-05-24 16:51:08 -0300

    added build.hxml

--- src/Main.hx
+++ src/Main.hx
@@ -3,36 +3,35 @@ package ;
 import github.Api;
 import mcli.CommandLine;
 import mcli.Dispatch;
-import neko.Lib;
 
 /**
- * 
+ *
  * Google Code to Github issues importer
- * 
+ *
  * Usage: issueimport [options] <token> <google-code-repo> <github-project>
  * .  google-code-repo - name of the google code repo (e.g. haxe)
  * .  github           - path of github project (e.g. HaxeFoundation/Haxe)
- * 
+ *
  */
 class Main extends CommandLine
 {
-	static function main() 
+	static function main()
 	{
 		new Dispatch(Sys.args()).dispatch(new Main());
 	}
-	
+
 	/**
 	 * verbose mode
 	 * @alias v
 	 */
 	public var verbose:Bool;
-	
+
 	/**
 	 * github OAuth token
 	 * @alias t
 	 */
 	public var token:String;
-	
+
 	/**
 	 * shows this message
 	 */
@@ -41,7 +40,7 @@ class Main extends CommandLine
 		Sys.println(this.showUsage());
 		Sys.exit(1);
 	}
-	
+
 	public function runDefault(googleCode:String, github:String)
 	{
 		if (token == null)
@@ -59,20 +58,20 @@ class Main extends CommandLine
 			warn(t.message);
 			Sys.exit(1);
 		}
-		
+
 	}
-	
+
 	private function p(s:String)
 	{
 		Sys.println(s);
 	}
-	
+
 	private function log(s:String)
 	{
 		if (verbose)
 			Sys.println(s);
 	}
-	
+
 	private function warn(s:String)
 	{
 		Sys.stderr().writeString(s + "\n");
